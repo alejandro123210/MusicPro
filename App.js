30d6c956f9da0d067770566f07e787d37919a93a
Alejandro Gonzalez <myname123210@gmail.com>
2020-02-01 14:16:04 -0500

    added new notification api

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Router, Scene, Tabs } from 'react-native-router-flux';
-import PushNotificationIOS from "@react-native-community/push-notification-ios";
 import { Image, View } from 'react-native'
 
 //login views
