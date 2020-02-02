f5e8e103acfc9ad7f9786089866d2b7c66e1b9e0
Alejandro Gonzalez <myname123210@gmail.com>
2020-02-02 13:17:30 -0500

    set up some notification stuff

--- android/app/src/main/java/com/musicpro/MainApplication.java
+++ android/app/src/main/java/com/musicpro/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.wix.reactnativenotifications.RNNotificationsPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           new RNGoogleSigninPackage();
+          packages.add(new RNNotificationsPackage(MainApplication.this)); 
           return packages;
         }
 
--- views/StudentViews/StudentDash.js
+++ views/StudentViews/StudentDash.js
@@ -2,15 +2,30 @@
 //has to 3 link buttons but two of them link to the same screen for now
 import React from "react";
 import LessonList from "../subComponents/LessonList";
+import {Notifications} from 'react-native-notifications'
 
 class StudentDash extends React.Component {
 
+  componentDidMount(){
+    Notifications.registerRemoteNotifications();
+
+    Notifications.events().registerNotificationReceivedForeground((notification, completion) => {
+      console.log(`Notification received in foreground: ${notification.title} : ${notification.body}`);
+      completion({alert: false, sound: false, badge: false});
+    });
+
+    Notifications.events().registerNotificationOpened((notification, completion) => {
+      console.log(`Notification opened: ${notification.payload}`);
+      completion();
+    });
+  }
+
   render() {
     return (
-      <LessonList 
+      <LessonList
         userData = {this.props.userData}
         lessonType = 'confirmed'
-      />      
+      />
     );
   }
 }
